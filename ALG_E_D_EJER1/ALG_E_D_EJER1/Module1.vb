0aa89e796219190d0ccbe7652f83a20cfc1a768e
usuario <usuario@LABE01>
2017-08-25 14:12:03 -0500

    CLASE 1 DE ALGORITMOS Y ESTRUCTURA DE DATOS 28-08-2017 ALGORITMO DE FECHA Y DEVUELVE LA FECHA EN UN ORACION COMPLETA

---
+++ ALG_E_D_EJER1/ALG_E_D_EJER1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+
+        Dim aa, dd, mm, tipo As Integer
+        Console.WriteLine("PROGRAMA QUE NOS DA LA FECHA EN TRES DIFERENTES TIPOS")
+        Console.WriteLine("---------------------------------------------------------------------")
+        Console.Write("INGRESE EL DIA EN DOS DIGITO: ")
+        dd = Console.ReadLine()
+        Console.Write("INGRESE EL MES EN DOS DIGITO: ")
+        mm = Console.ReadLine()
+        Console.Write("INGRESE EL AÑO EN DOS DIGITO: ")
+        aa = Console.ReadLine()
+        Console.WriteLine("INGRESE EL TIPO DE FECHA QUE QUIERE VER")
+        Console.WriteLine("TIPO 1 ===> 25 DE AGOSTO DE 2017")
+        Console.WriteLine("TIPO 2 ===> AGOSTO 25, 2017")
+        Console.WriteLine("TIPO 3 ===> VIERNES 25 DE AGOSTO")
+        tipo = Console.ReadLine()
+        Console.WriteLine("LA FECHA ACTUAL ES : " + fechas(dd, mm, aa, tipo))
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ALG_E_D_EJER1/ALG_E_D_EJER1/Module2.vb
@@ -0,0 +1,69 @@
+﻿Module Module2
+    Function fechas(dd, mm, aa, tipo) As String
+        Dim aux1, aux2, conta, resto As Integer
+        Dim ultimeses(12) As Integer
+        Dim meses(12), lafecha, dias(7) As String
+        conta = 1
+        ultimeses(1) = 31
+        ultimeses(2) = 28
+        ultimeses(3) = 31
+        ultimeses(4) = 30
+        ultimeses(5) = 31
+        ultimeses(6) = 30
+        ultimeses(7) = 31
+        ultimeses(8) = 31
+        ultimeses(9) = 30
+        ultimeses(10) = 31
+        ultimeses(11) = 30
+        ultimeses(12) = 31
+        meses(1) = "Enero"
+        meses(2) = "Febrero"
+        meses(3) = "Marzo"
+        meses(4) = "Abril"
+        meses(5) = "Mayo"
+        meses(6) = "Junio"
+        meses(7) = "Julio"
+        meses(8) = "Agosto"
+        meses(9) = "Setiembre"
+        meses(10) = "Octubre"
+        meses(11) = "Noviembre"
+        meses(12) = "Diciembre"
+        dias(1) = "Domingo"
+        dias(2) = "Lunes"
+        dias(3) = "Martes"
+        dias(4) = "Miercoles"
+        dias(5) = "Jueves"
+        dias(6) = "Viernes"
+        dias(7) = "Sabado"
+        aux1 = (aa / 4)
+        aux2 = aux1 * 4
+        If aux2 = aa Then
+            ultimeses(2) = 29
+        Else
+            ultimeses(2) = 28
+        End If
+
+        If tipo = 1 Then
+            lafecha = Str(dd) + " de " + meses(mm) + " de" + Str(aa + 2000)
+        Else
+            If tipo = 2 Then
+                lafecha = meses(mm) + Str(dd) + ", " + Str(aa + 2000)
+            Else
+                If tipo = 3 Then
+                    aux1 = ((aa - 1) * 365)
+                    aux1 = aux1 + Int((aa - 1) / 4)
+                    Do Until conta = mm
+                        aux1 = aux1 + ultimeses(conta)
+                        conta = conta + 1
+                    Loop
+                    aux1 = aux1 + dd
+                    aux2 = Int(aux1 / 7)
+                    resto = (aux1 - (aux2 * 7)) + 1
+                    lafecha = dias(resto) + Str(dd) + " de " + meses(mm)
+                End If
+            End If
+        End If
+        Return lafecha
+
+    End Function
+End Module
