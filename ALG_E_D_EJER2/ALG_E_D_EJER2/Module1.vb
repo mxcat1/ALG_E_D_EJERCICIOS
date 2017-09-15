a9171c8045c2363fc111a3b1e4bc5f97f3944290
usuario <usuario@labe05>
2017-09-15 14:24:12 -0500

    CLASE 15/09/2017

--- ALG_E_D_EJER2/ALG_E_D_EJER2/Module1.vb
+++ ALG_E_D_EJER2/ALG_E_D_EJER2/Module1.vb
@@ -2,16 +2,20 @@
 
     Sub Main()
         Dim a As Integer
-        Dim l(100), opcion As String
+        Dim opcion As String
+        Dim canti_datos As Integer
+        Console.WriteLine("INDIQUE LA CANTIDAD DE DATOS QUE VA A INGRESAR")
+        canti_datos = Console.ReadLine()
+        Dim l(canti_datos) As String
         'PROCEDIMIENTO QUE AGREGA VALORES ALA LISTA
-        agregar(l)
+        agregar(l, canti_datos)
         Console.WriteLine("ELIJA COMO SE VA A MOSTRAR LA LISTA EN ASCENDENTE 'A' O EN DESCENDENTE 'D' O SOLO ENTER")
         'VARIABLE QUE TE PERMITE ELEGIR LA OPCION PARA QUE SE MUESTRE LA LISTA
         opcion = Console.ReadLine
         'FUNCION QUE ORDENE LA LISTA Y USS DATOS
-        l = ordenar(l, opcion)
+        l = ordenar(l, opcion, canti_datos)
         'PROCEDIMIENTO QUE MUESTRA LA LISTA
-        mostrar(l)
+        mostrar(l, canti_datos)
         Console.ReadLine()
     End Sub
 
--- ALG_E_D_EJER2/ALG_E_D_EJER2/Module2.vb
+++ ALG_E_D_EJER2/ALG_E_D_EJER2/Module2.vb
@@ -1,14 +1,14 @@
 ﻿Module Module2
     Dim a As Integer = 1
-    Function ordenar(l, opcion) As Array 'FUNCION QUE ORDENA DESCENDENTEMENTE o ASCENDENTEMENTE UNA LISTA DE NOMBRES
+    Function ordenar(l, opcion, n_datos) As Array 'FUNCION QUE ORDENA DESCENDENTEMENTE o ASCENDENTEMENTE UNA LISTA DE NOMBRES
 
         Dim a, b As Integer
         Dim t As String
         a = 1
         b = 2
 
-        Do Until a = 100 Or l(a) = ""
-            Do Until b = 101 Or l(b) = ""
+        Do Until a = n_datos Or l(a) = ""
+            Do Until b = n_datos Or l(b) = ""
                 If opcion = "A" Or opcion = "a" Then
                     If l(a) < l(b) And (l(a) <> "" Or l(b) <> "") Then
                         t = l(a)
@@ -32,12 +32,12 @@
         Return l
 
     End Function
-    Public Sub mostrar(l()) 'PROCEDIMIENTO QUE MUESTRA LA LISTA DE NOMBRES  
+    Public Sub mostrar(l, n_datos) 'PROCEDIMIENTO QUE MUESTRA LA LISTA DE NOMBRES  
         a = 1
-        Do While a < 101
+        Do While a < n_datos + 1
 
             If l(a) = "" Then
-                a = 100
+                a = n_datos
             Else
                 Console.WriteLine(Str(a) + "------->" + l(a))
             End If
@@ -45,14 +45,14 @@
             a = a + 1
         Loop
     End Sub
-    Public Sub agregar(l())
+    Public Sub agregar(l(), n_datos)
         a = 1
         Console.WriteLine("INGRESE LOS VALORES A ORDENAR")
-        Do While a < 101
+        Do While a < n_datos + 1
             Console.Write(Str(a) + "-----> ")
             l(a) = Console.ReadLine
             If l(a) = "" Then
-                a = 100
+                a = n_datos
             End If
             a = a + 1
         Loop
