f0e3455f75c8a5cb29c2c90ce96ccae23c64c48c
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-09-28 22:16:29 -0500

    CASA

--- ALG_E_D_EJERCICIO_PRACTICA/ALG_E_D_EJERCICIO_PRACTICA/Module1.vb
+++ ALG_E_D_EJERCICIO_PRACTICA/ALG_E_D_EJERCICIO_PRACTICA/Module1.vb
@@ -1,46 +1,56 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim lista(10), valorre, burbuja As String
-        Dim x, y, c As Integer
+        Dim tamaño As String
+        Console.Write("INDIQUE EL TAMAÑO DE LA VARIABLE INDEXADA A VERIFICAR ====> ")
+        tamaño = Console.ReadLine()
 
-        lista(1) = "bb"
-        lista(2) = "bb"
-        lista(3) = "aa"
-        lista(4) = "cc"
-        lista(5) = "dd"
-        lista(6) = "gg"
-        lista(7) = "aa"
-        lista(8) = "aa"
-        lista(9) = "aa"
-        lista(10) = "ff"
-        x = 1
-        y = 2
-        c = 1
-        Do Until x = 10
-            Do Until y = 11 Or lista(x) = ""
-                If lista(x) = lista(y) Then
-                    lista(y) = ""
-                    c = y
-                    Do While c <> 10
-                        burbuja = lista(c)
-                        lista(c) = lista(c + 1)
-                        lista(c + 1) = burbuja
-                        c = c + 1
-                    Loop
-                End If
-                If lista(x) <> lista(y) Then
-                    y = y + 1
-                End If
-            Loop
-            x = x + 1
-            y = x + 1
-        Loop
-        x = 1
-        Do While x < 11
-            Console.WriteLine(lista(x))
-            x = x + 1
-        Loop
+        Dim lista(tamaño) As String
+
+
+
+        '*******CODIGO BASE
+        ', valorre, burbuja As String
+        'Dim x, y, c As Integer
+
+        'lista(1) = "bb"
+        'lista(2) = "bb"
+        'lista(3) = "aa"
+        'lista(4) = "cc"
+        'lista(5) = "dd"
+        'lista(6) = "gg"
+        'lista(7) = "aa"
+        'lista(8) = "aa"
+        'lista(9) = "aa"
+        'lista(10) = "ff"
+        'x = 1
+        'y = 2
+        'c = 1
+        'Do Until x = 10
+        '    Do Until y = 11 Or lista(x) = ""
+        '        If lista(x) = lista(y) Then
+        '            lista(y) = ""
+        '            c = y
+        '            Do While c <> 10
+        '                burbuja = lista(c)
+        '                lista(c) = lista(c + 1)
+        '                lista(c + 1) = burbuja
+        '                c = c + 1
+        '            Loop
+        '        End If
+        '        If lista(x) <> lista(y) Then
+        '            y = y + 1
+        '        End If
+        '    Loop
+        '    x = x + 1
+        '    y = x + 1
+        'Loop
+        'x = 1
+        'Do While x < 11
+        '    Console.WriteLine(lista(x))
+        '    x = x + 1
+        'Loop
+        'CODIGO BASE FIN ********
         Console.ReadLine()
     End Sub
 
--- ALG_E_D_EJERCICIO_PRACTICA/ALG_E_D_EJERCICIO_PRACTICA/Module2.vb
+++ ALG_E_D_EJERCICIO_PRACTICA/ALG_E_D_EJERCICIO_PRACTICA/Module2.vb
@@ -1,3 +1,29 @@
 ﻿Module Module2
-
+    Function borrar_repetidos(lista, tamano) As Array
+        Dim burbuja As String
+        Dim x, y, c As Integer
+        x = 1
+        y = 2
+        c = 1
+        Do Until x = tamano
+            Do Until y = tamano + 1 Or lista(x) = ""
+                If lista(x) = lista(y) Then
+                    lista(y) = ""
+                    c = y
+                    Do While c <> tamano
+                        burbuja = lista(c)
+                        lista(c) = lista(c + 1)
+                        lista(c + 1) = burbuja
+                        c = c + 1
+                    Loop
+                End If
+                If lista(x) <> lista(y) Then
+                    y = y + 1
+                End If
+            Loop
+            x = x + 1
+            y = x + 1
+        Loop
+        Return lista
+    End Function
 End Module
